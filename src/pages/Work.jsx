9e42f256e22df19a492bf1edd493463715b774dd
Balexm95 <bria.mccoy95@gmail.com>
2022-04-25 21:15:24 -0400

    portfolio link added

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -154,12 +154,12 @@ const Work = () => {
                 My Portfolio
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://portfolio-fi9wgxcc1-balexm95.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/Balexm95/Portfolio'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
